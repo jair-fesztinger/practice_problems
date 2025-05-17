b78e3e6f90d4a3e591e7f6d51ce8d4d6fabc2562
Jonathan Napoles <napoles.jair@gmail.com>
2025-05-16 22:24:09 -0500

    Time: 263 ms (65.50%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,10 +1,12 @@
 -- Write your PostgreSQL query statement below
-SELECT  DISTINCT num as ConsecutiveNums
-FROM (
+WITH lagged AS (
     SELECT 
     num,
     lag(num, 1) OVER (ORDER BY id) AS first_check,
     lag(num, 2) OVER (ORDER BY id) AS second_check
     FROM Logs
-) AS filtered_table
+)
+
+SELECT  DISTINCT num as ConsecutiveNums
+FROM lagged
 WHERE num = first_check AND num = second_check;
